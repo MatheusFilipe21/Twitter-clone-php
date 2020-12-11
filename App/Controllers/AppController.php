aa402401e1a2d64983a238a9d219ed1c042b15c1
MatheusFilipe21 <matheusfilipe1999@gmail.com>
2020-12-11 19:11:47 -0300

    Criando a timeline, incluindo e listando tweets

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -3,19 +3,43 @@
 namespace App\Controllers;
 
 use MF\Controller\Action;
+use MF\Model\Container;
 
 class AppController extends Action{
 
     public function timeline(){
+        $this->validateAuth();
+            
+        $tweet = Container::getModel('tweet');
+
+        $tweet->__set('id_usuario', $_SESSION['id']);
+
+        $tweets = $tweet->getAll();
+
+        $this->view->tweets = $tweets;
+        
+        $this->render('timeline');
+    }
+
+    public function tweet(){
+        $this->validateAuth();
+        
+        $tweet = Container::getModel('tweet');
+        
+        $tweet->__set('id_usuario', $_SESSION['id']);
+        $tweet->__set('tweet', $_POST['tweet']);
+
+        $tweet->save();
+
+        header('Location: /timeline');
+    }
+
+    public function validateAuth(){
         session_start();
-
-        if (!empty($_SESSION['id']) && !empty($_SESSION['nome'])) {
-            $this->render('timeline');
-
-        } else {
+        
+        if (empty($_SESSION['id']) || empty($_SESSION['nome'])) {
             header('Location: /?login=erro');
         }
-
     }
 
 }
